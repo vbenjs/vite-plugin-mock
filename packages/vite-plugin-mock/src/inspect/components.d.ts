2115f7dc21b391066397cf6c09173ec56e9dc08d
zcf0508 <zcf0508@live.com>
2023-05-08 19:10:59 +0800

    feat: add `excludeAll` and `includeAll`

--- packages/vite-plugin-mock/src/inspect/components.d.ts
+++ packages/vite-plugin-mock/src/inspect/components.d.ts
@@ -9,6 +9,7 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    ElButton: typeof import('element-plus/es')['ElButton']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElInput: typeof import('element-plus/es')['ElInput']
--- packages/vite-plugin-mock/src/inspect/home.vue
+++ packages/vite-plugin-mock/src/inspect/home.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import ky from 'ky'
-import { watch, computed, ref } from 'vue'
-import { useMockList } from './hooks/useMockList'
+import { computed, ref } from 'vue'
+import { MockObj, useMockList } from './hooks/useMockList'
 
-const { mockList } = useMockList()
+const { mockList, getList } = useMockList()
 
 const filterPath = ref('')
 
@@ -13,31 +13,68 @@ const filterMockList = computed(()=>{
   })
 })
 
-const excludeUrl = computed(()=>{
-  return mockList.value.filter(i => !i.include).map(i=> `${i.url}+${i.method || 'get'}`)
-})
+async function change(mock: MockObj) {
+  if(mock.include) {
+    mockList.value.map(i => {
+      if(i.url === mock.url && i.method === mock.method) {
+        i.include = false
+        i.exclude = true
+      }
+    })
+  } else {
+    mockList.value.map(i => {
+      if(i.url === mock.url && i.method === mock.method) {
+        i.include = true
+        i.exclude = false
+      }
+    })
+  }
 
-watch(() => excludeUrl.value, () => {
-  ky.post('./exclude', {
+  await ky.post('./exclude', {
     json: {
-      "urlList": excludeUrl.value
+      "urlList": mockList.value.filter(i => i.exclude).map(i => `${i.url}+${i.method || 'get'}`)
     }
   })
-})
+  await getList()
+}
 
+async function excludeAll() {
+  await ky.post('./exclude', {
+    json: {
+      "urlList": mockList.value.map(i => `${i.url}+${i.method || 'get'}`)
+    }
+  })
+  await getList()
+}
+
+async function includeAll() {
+  await ky.post('./exclude', {
+    json: {
+      "urlList": []
+    }
+  })
+  await getList()
+}
 
 </script>
 
 <template>
-  <el-form :inline="true">
-    <el-form-item label="Path">
-      <el-input v-model="filterPath" clearable></el-input>
-    </el-form-item>
-  </el-form>
+  <div class="sticky top-0 z-10 bg-white mb-2">
+    <el-form :inline="true">
+      <el-form-item label="Filter">
+        <el-input v-model="filterPath" placeholder="Input Path" clearable></el-input>
+      </el-form-item>
+      <el-form-item label="Other">
+        <el-button @click="excludeAll">Exclude All</el-button>
+      <el-button @click="includeAll">Include All</el-button>
+      </el-form-item>
+    </el-form>
+    
+  </div>
   <el-table :data="filterMockList">
-    <el-table-column label="Status">
+    <el-table-column label="Status" width="200">
       <template #default="scope">
-        <el-switch :value="scope.row.include" @click="scope.row.include = !scope.row.include"></el-switch>
+        <el-switch :value="scope.row.include" @click="change(scope.row)"></el-switch>
       </template>
     </el-table-column>
     <el-table-column label="Path" prop="url"></el-table-column>
